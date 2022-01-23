269dd5fe09a84406970111b3a861cda4beda6468
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-01-23 15:17:17 -0500

    working

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -9,35 +9,41 @@ const Mainpage = () => {
     const [answer, setAnswer] = useState('')
     const [error, setError] = useState('')
         
+    const isValidCalculation = () =>
+        !Number.isNaN(Number(numOne)) &&
+        !Number.isNaN(Number(numTwo)) &&
+        numOne !== "" &&
+        numTwo !== "";
 
     const calculate = () => {
-        if(!numOne && !numTwo){
-           // setAnswer('')
-            return setError('Invalid input, try again')
+        if (!isValidCalculation()) {
+            setAnswer('');
+            return setError('Invalid input, try again');
         }
-        //setError('')
-        //setAnswer('')
+        setError('');
+        setAnswer('');
 
+    
         switch (operation) {
            case '+':
-               return setAnswer((Number(numOne)) + (Number(numTwo)))
+               return setAnswer((Number(numOne)) + (Number(numTwo)));
            case '-':
-               return setAnswer((Number(numOne)) - (Number(numTwo)))
+               return setAnswer((Number(numOne)) - (Number(numTwo)));
            case '*':
-               return setAnswer((Number(numOne)) * (Number(numTwo)))
+               return setAnswer((Number(numOne)) * (Number(numTwo)));
            case '/':
-               return setAnswer((Number(numOne)) / (Number(numTwo)))
-            default: 
-                return setError('Invalid input, try again')
+               return setAnswer((Number(numOne)) / (Number(numTwo)));
+            default:  
+               return setError('Invalid input, try again');
        }          
-    }
+    };
 
     return(
         <div className= "mainpage">
         <Title />
 
             <div className="Inputs">
-                <input type="text" value={numOne} onChange={(event) => setNumOne(event.target.value)} />
+                <input name="numOne" value={numOne} onChange={(event) => setNumOne(event.target.value)} />
 
                 <select value={operation} onChange={(event) => setOperation(event.target.value)}>
                     <option value="+"> + </option>
@@ -46,18 +52,15 @@ const Mainpage = () => {
                     <option value="/"> / </option>
                 </select>
 
-                <input type="text" value={numTwo} onChange={(event) => setNumTwo(event.target.value)} />
+                <input name="numTwo" value={numTwo} onChange={(event) => setNumTwo(event.target.value)} />
 
                 <button type="button" onClick={calculate}> =</button>
 
-                <input type="text" value={answer}/>
-
-                <div>{error}</div>
-           
-           
+                <input name="answer" value={answer} />
 </div>
+        <div calssName="error">{error}</div>
 </div> 
-           )
-}
+           );
+};
 
 export default Mainpage
